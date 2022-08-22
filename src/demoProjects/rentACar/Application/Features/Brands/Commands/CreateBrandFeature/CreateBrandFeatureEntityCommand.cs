0e162062e2003b234881aa29acb6b650eed824b6
dagci <dagci@TR-ADN-13SZ633>
2022-08-22 23:07:27 +0300

    dev

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrandFeature/CreateBrandFeatureEntityCommand.cs
@@ -0,0 +1,32 @@
+﻿using Application.Features.Brands.Dtos;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.CreateBrandFeature
+{
+    public class CreateBrandFeatureEntityCommand : IRequest<CreatedBrandFeatureEntityDto>
+    {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+        public CreateBrandFeatureEntityCommand(IMapper mapper, IBrandRepository brandRepository)
+        {
+            _mapper = mapper;
+            _brandRepository = brandRepository;
+        }
+        public async Task<CreatedBrandFeatureEntityDto> Handle(CreateBrandFeatureEntityCommand request, CancellationToken cancellationToken)
+        {
+
+            Brand mappedSomeFeatureEntity = _mapper.Map<Brand>(request);
+            Brand createdBrandFeatureEntity = await _brandRepository.AddAsync(mappedSomeFeatureEntity);
+            CreatedBrandFeatureEntityDto createdSomeFeatureEntityDto = _mapper.Map<CreatedBrandFeatureEntityDto>(createdBrandFeatureEntity);
+            return createdSomeFeatureEntityDto;
+        }
+    }
+}
