91e08493148798dd5af9f49daa8aedd12ccaebb1
dagci <dagci@TR-ADN-13SZ633>
2022-08-23 08:12:22 +0300

    controller

---
+++ src/demoProjects/rentACar/WebAPI/Controllers/BrandsController.cs
@@ -0,0 +1,20 @@
+﻿using Application.Features.Brands.Commands.CreateBrandFeature;
+using Application.Features.Brands.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BrandsController : BaseController
+    {      
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateBrandFeatureEntityCommand createBrandFeatureEntityCommand)
+        {
+            CreatedBrandFeatureEntityDto result = await Mediator.Send(createBrandFeatureEntityCommand);
+            return Created("", result);
+        }
+
+
+    }
+}
